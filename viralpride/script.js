f078810307b1598c7dede25bb632e01b42eeb798
viralpride <36273273+Eizza@users.noreply.github.com>
2023-12-30 21:17:58 +0600

    Add files via upload

--- viralpride/script.js
+++ viralpride/script.js
@@ -26,7 +26,7 @@ window.onload = function(){
 		s: 59,
 	}
 	// ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-	var countDownDate = new Date("Nov 25, 2023 23:59:59").getTime();
+	var countDownDate = new Date("Jan 3, 2024 23:59:59").getTime();
 
 	timerF();
 
@@ -195,6 +195,10 @@ function draw(){
 		window.requestAnimationFrame(loop);
 	}
 }
+
+// ====================SNOW=====================
+
+
 
 window.onresize = function(){
 	opts.w = window.innerWidth;
